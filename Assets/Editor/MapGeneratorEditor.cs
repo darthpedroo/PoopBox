c298daf1bcdd0e7ed8e57b0080ad0919003235e2
gazoil2 <bverdun@alumno.huergo.edu.ar>
2024-08-08 01:16:48 -0300

    added simple mesh

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -1,20 +1,39 @@
-using System.Collections;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(MapGenerator))]
 public class MapGeneratorEditor : Editor
 {
-    public override void OnInspectorGUI(){
+    public override void OnInspectorGUI()
+    {
         MapGenerator mapGen = (MapGenerator)target;
 
-        if (DrawDefaultInspector ()){
-            if (mapGen.autoUpdate){
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
                 mapGen.GenerateMap();
             }
         }
-        if (GUILayout.Button("Generate")){
+        if (GUILayout.Button("Generate"))
+        {
             mapGen.GenerateMap();
         }
+    }
+}
+
+[InitializeOnLoad]
+public static class MapGeneratorInitializer
+{
+    static MapGeneratorInitializer()
+    {
+        EditorApplication.delayCall += OnEditorReload;
+    }
+
+    private static void OnEditorReload()
+    {
+        // Find the MapGenerator instance in the scene
+        MapGenerator mapGen = GameObject.FindObjectOfType<MapGenerator>();
+        mapGen?.GenerateMap();
     }
 }
