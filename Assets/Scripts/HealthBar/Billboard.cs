02c2cc401453f53237e36028187dc74feb1519e4
Ansa04 <sansa@alumno.huergo.edu.ar>
2024-08-22 20:19:21 -0300

    Changed healthabr to use facing direction of the player and distance

--- Assets/Scripts/HealthBar/Billboard.cs
+++ Assets/Scripts/HealthBar/Billboard.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     public Transform cam;
+    private Canvas healthBarCanvas;
     void Start(){
         cam = FindObjectOfType<Camera>().transform;
         Vector3 gyat = transform.parent.parent.position;
         gyat.y += 6f;
         transform.parent.parent.position = gyat;
+        healthBarCanvas = GetComponentInParent<Canvas>();
     }
 
     void LateUpdate(){
+        Vector3 dir = (transform.position - cam.position).normalized;
+        float dot = Vector3.Dot(dir, transform.forward);
         transform.LookAt(transform.position + cam.forward);
+        healthBarCanvas.enabled = (cam.position - transform.position).magnitude < 30f && dot >= 0.95;
     }
 }
