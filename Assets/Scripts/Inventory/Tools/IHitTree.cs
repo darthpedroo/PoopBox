18331214c071470cbf8cb4e50648ff0d0dfa0096
gazoil2 <bverdun@alumno.huergo.edu.ar>
2024-08-22 23:27:17 -0300

    obsolete

--- Assets/Scripts/Inventory/Tools/IHitTree.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IHitTree 
-{
-    // Interfaz para las herramientas que puedan golpear arboles
-    int HitTree();
-}
