6a0600cb035717421861021bfa942b7878ad8213
darthpedroo <pedrovillarino06@gmail.com>
2024-08-26 22:07:58 -0300

    added wood mesh

--- Assets/Scripts/Inventory/Materials/Wood.cs
+++ Assets/Scripts/Inventory/Materials/Wood.cs
@@ -13,9 +13,18 @@ public class Wood : Resource
     }
     public override void equipItem(GameObject parentObject) {
         Debug.Log("Currently equipped: [Wood x" + Count + "]");
+        GameObject axeObject = ObjectInstantiator.InstantiatePrefab("Prefabs/Wood", new Vector3(0, 0, 0), Quaternion.Euler(0f, 0f, 0f));
+        axeObject.transform.parent = parentObject.transform;
+        axeObject.transform.position = parentObject.transform.position;
+        axeObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        axeObject.layer = LayerMask.NameToLayer("holdLayer");
+        meshFilter = axeObject.GetComponent<MeshFilter>();
+        meshRenderer = axeObject.GetComponent<MeshRenderer>();
+        
     }
     public override void createObject()
     {
         itemTexture = Resources.Load<Texture>("wood");
+        
     }
 }
